09fe68be53b179b8024ca6c0de63968920418381
cristianbournissent <cristianbournissent09@gmail.com>
2023-10-30 10:49:01 -0300

    first commit

---
+++ src/components/hooks/useFetchGifs.js
@@ -0,0 +1,27 @@
+//hooks son funciones
+
+import { useEffect, useState } from 'react';
+import { getGifs } from '../../helpers/getGifs';
+
+    export const useFetchGifs = (category) =>{
+
+         const [images, setImages] = useState ([]);
+         const [isLoading, setIsLoading] = useState(true);
+
+        const getImages = async () =>{
+        const newImages = await getGifs (category);
+        setImages (newImages);
+        setIsLoading(false);
+    }
+
+    useEffect(()=>{
+        getImages();
+    }, [])
+    
+  
+        return{
+            images,
+            isLoading
+        }
+
+    }
